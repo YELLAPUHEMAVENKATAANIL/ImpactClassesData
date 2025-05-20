791a7ddda980476b456c53d053dae780541bfc84
YELLAPUHEMAVENKATA <anilyallapu12345@gmail.com>
2025-05-20 15:19:19 +0530

    Initial Commit

---
+++ Day4Classes/NextGreaterNumber.java
@@ -0,0 +1,13 @@
+package Day4Classes;
+
+import java.util.Scanner;
+
+public class NextGreaterNumber {
+    Scanner sc = new Scanner(System.in);
+    
+    long number = sc.nextLong();
+
+    
+
+    
+}
